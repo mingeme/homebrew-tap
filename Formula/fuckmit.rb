9809bdf191e1d23cd89deab5d11914968b841c0d
mingeme <heminghu00@126.com>
2025-04-14 16:30:24 +0800

    fix: update url and version references in fuckmit formula
    Updated the URL and version references in the Formula/fuckmit.rb file to dynamically use the `version` variable instead of hardcoding "0.1.0". This includes:
    - Changing the download URLs for both ARM64 and AMD64 architectures
    - Updating the binary installation paths
    - Modifying the test to assert the correct version dynamically
    
    This change ensures the formula remains up-to-date as new versions are released.

--- Formula/fuckmit.rb
+++ Formula/fuckmit.rb
@@ -6,21 +6,21 @@ class Fuckmit < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mingeme/fuckmit/releases/download/v0.1.0/fuckmit-macos-arm64-0.1.0"
+      url "https://github.com/mingeme/fuckmit/releases/download/v#{version}/fuckmit-macos-arm64-#{version}"
       sha256 "841dd5f66890364091b36b95cea7e7ca81b8f57055767dc4e7de11c9aabcfd7c"
     elsif Hardware::CPU.intel?
-      url "https://github.com/mingeme/fuckmit/releases/download/v0.1.0/fuckmit-macos-amd64-0.1.0"
+      url "https://github.com/mingeme/fuckmit/releases/download/v#{version}/fuckmit-macos-amd64-#{version}"
       sha256 "e0251feb72129da3135edbba72ad3e2cb5d6e61ef8ae4c2eef0c074e78b20103"
     end
   end
 
   def install
-    bin.install "fuckmit-macos-arm64-0.1.0" => "fuckmit" if Hardware::CPU.arm?
-    bin.install "fuckmit-macos-amd64-0.1.0" => "fuckmit" if Hardware::CPU.intel?
+    bin.install "fuckmit-macos-arm64-#{version}" => "fuckmit" if Hardware::CPU.arm?
+    bin.install "fuckmit-macos-amd64-#{version}" => "fuckmit" if Hardware::CPU.intel?
     chmod 0755, bin/"fuckmit"
   end
 
   test do
-    assert_match "fuckmit 0.1.0", shell_output("#{bin}/fuckmit --version")
+    assert_match "fuckmit #{version}", shell_output("#{bin}/fuckmit --version")
   end
 end
