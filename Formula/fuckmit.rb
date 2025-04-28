fcd091d327303feb87357e6e135bf62c43ea8866
mingeme <heminghu00@126.com>
2025-04-28 09:51:08 +0800

    fuckmit 0.3.0-alpha.1

--- Formula/fuckmit.rb
+++ Formula/fuckmit.rb
@@ -1,16 +1,16 @@
 class Fuckmit < Formula
   desc "AI-powered git commit message generator"
   homepage "https://github.com/mingeme/fuckmit"
-  version "0.2.1"
+  version "0.3.0-alpha.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mingeme/fuckmit/releases/download/v#{version}/fuckmit-darwin-aarch64-#{version}.tar.gz"
-      sha256 "869a8be185d5e30c19afc535f8b99a34f73e173c520531dbed8dc6f587d16f2a"
+      sha256 "8c7584830792f56227cd64ef1f5145effae86931f55140c32cfe8eeafccbb933"
     elsif Hardware::CPU.intel?
       url "https://github.com/mingeme/fuckmit/releases/download/v#{version}/fuckmit-darwin-x86_64-#{version}.tar.gz"
-      sha256 "8105f2753ab0261c15e46fa578220d1ac676906fc3eec9e6f5d347e33ec5f899"
+      sha256 "fc90616f64805348886c60db622f5c448948d7d6983b3e72a41368e905957163"
     end
   end
 
