c8870e5bcb8a559011264ad3ea08c296d3b97bde
mingeme <heminghu00@126.com>
2025-06-22 22:47:14 +0800

    fix sha256 sum

--- Formula/fuckmit.rb
+++ Formula/fuckmit.rb
@@ -7,10 +7,10 @@ class Fuckmit < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mingeme/fuckmit/releases/download/v#{version}/fuckmit-darwin-aarch64-#{version}.tar.gz"
-      sha256 "8c7584830792f56227cd64ef1f5145effae86931f55140c32cfe8eeafccbb933"
+      sha256 "dcd1bf3423a5252dee14fbaadedf7c7141b01b6f55842580a5d07431edc451ef"
     elsif Hardware::CPU.intel?
       url "https://github.com/mingeme/fuckmit/releases/download/v#{version}/fuckmit-darwin-x86_64-#{version}.tar.gz"
-      sha256 "fc90616f64805348886c60db622f5c448948d7d6983b3e72a41368e905957163"
+      sha256 "2e93078aac4af4e1e3547e5cb54e091d25af870ce74530fab6cc4333a57826cd"
     end
   end
 
